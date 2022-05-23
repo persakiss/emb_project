ae15f24812ed88543ea78856f088816b9401737e
Arman <persakiss@gmail.com>
2022-05-23 13:43:59 +0400

    Getting too focused on Carousel, not enough on design

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -3,6 +3,12 @@
 // You have two choices for including Bootstrap's JS files—the whole thing,
 // or just the bits that you need.
 
+$(document).ready(function () {
+    console.log($('#carouselwithIndicators').css('height')); // check the initial height of the carousel;
+
+    // now apply this height as a max-height on all the image items; css will handle the rest;
+    $('#carouselwithIndicators').find('.carousel-item img').css('max-height', $('#carouselwithIndicators').css('height'))
+});
 
 // Option 1
 //
